ccf889b9ee1733ece68f0ad6026a3e69657b9d2c
Errorsia <Errorsia@outlook.com>
2025-12-03 15:46:00 +0800

    add get pid from process name

--- Jiv_logic.py
+++ Jiv_logic.py
@@ -1,6 +1,8 @@
 import ctypes
 import subprocess
-import win32gui, win32con
+
+import psutil
+import win32gui, win32con, win32api
 
 
 class JIVLogic:
@@ -25,3 +27,36 @@ class JIVLogic:
         win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST,
                               0, 0, 0, 0,
                               win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+
+    @staticmethod
+    def taskkill(process_name):
+        state = subprocess.run(['TASKKILL', '-F', '-IM', process_name, '-T']).returncode
+
+        if state == 0:
+            return True
+            # self.logger.info(f'The process ({process_name}) has been terminated (Return code {state})')
+
+        elif state == 128:
+            return False
+            # self.logger.warning(f'The process ({process_name}) not found (Return code {state})')
+
+        elif state == 1:
+            return False
+            # self.logger.warning(
+                # f'The process ({process_name}) could not be terminated (Return code {state})')
+
+        else:
+            return False
+            # self.logger.warning(f'Unknown Error (Return code {state})')
+
+    @staticmethod
+    def get_pid_form_process_name():
+        pid = None
+        pids = psutil.process_iter()
+        for p in pids:
+            if p.name().lower() == "studentmain.exe":
+                pid = psutil.Process(p.pid)
+                break
+
+        return pid
+
