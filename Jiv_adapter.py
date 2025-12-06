e51c6a2b4cf9bf81f6e0f6343e594f625fd5cf71
Errorsia <Errorsia@outlook.com>
2025-12-06 20:49:31 +0800

    add terminate adapter; differentiate adapters by type

--- Jiv_adapter.py
+++ Jiv_adapter.py
@@ -9,40 +9,48 @@ class AdapterManager(QObject):
 
     def __init__(self, logic, gui):
         super().__init__()
+        self.on_demand_adapters = {}
         self.logic = logic
         self.gui = gui
-        self.workers = []
 
-        self.threads = {}
+        self.lifelong_adapters = []
+        self.lifelong_threads = {}
+
+        self.terminate_adapter = None
 
         self.init_workers()
         self.start_all()
 
     def init_workers(self):
-        self.workers.append(MonitorAdapter(1000, self.logic))
-        # self.workers.append(TopMostAdapter(100, self.gui))
-        # self.workers.append(DatabaseAdapter(logic, 2000))
-        # self.workers.append(NetworkAdapter(logic, 5000))
+        self.lifelong_adapters.append(MonitorAdapter(1000, self.logic))
+        # self.lifelong_adapters.append(TopMostAdapter(100, self.gui))
+        # self.lifelong_adapters.append(DatabaseAdapter(logic, 2000))
+        # self.lifelong_adapters.append(NetworkAdapter(logic, 5000))
+
+        self.terminate_adapter = TerminateAdapter(self.logic)
 
     def start_all(self):
-        for worker in self.workers:
+        for adapter in self.lifelong_adapters:
             thread = QThread()
-            worker.moveToThread(thread)
+            adapter.moveToThread(thread)
 
-            thread.started.connect(worker.start)
-            # 用 lambda 包装，把 worker 类名和结果一起发出去
-            worker.changed.connect(lambda result, w=worker:
+            thread.started.connect(adapter.start)
+            # Wrap with lambda and send the adapter class name and result together
+            adapter.changed.connect(lambda result, w=adapter:
                                    self.ui_change.emit(type(w).__name__, result))
 
-            self.threads[worker] = thread
+            self.lifelong_threads[adapter] = thread
             thread.start()
 
     def stop_all(self):
-        """停止所有 worker 并安全退出线程"""
-        for worker, thread in self.threads.items():
-            worker.stop()
+        """Stop all adapters and safely exit the thread"""
+        for adapter, thread in self.lifelong_threads.items():
+            adapter.stop()
             thread.quit()
             thread.wait()
+
+    def terminate_studentmain(self):
+        self.terminate_adapter.start()
 
 
 class BaseAdapterInterface:
@@ -111,3 +119,25 @@ class MonitorAdapter(QObject, BaseAdapterInterface):
 #
 #     def run_task(self):
 #         pass
+
+class TerminateAdapter(QObject):
+    change = Signal(bool)
+
+    def __init__(self, logic):
+        super().__init__()
+        self.logic = logic
+        self.last_result = None
+
+    def start(self):
+        self.run_task()
+
+    def run_task(self):
+        pid = self.logic.get_pid_form_process_name('studentmain.exe')
+        if pid is None:
+            print('studentmain not found')
+            return
+        self.logic.terminate_process(pid)
+
+    def check_state(self):
+        return self.logic.get_studentmain_state()
+
