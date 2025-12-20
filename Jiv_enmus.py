7cc6468efc954d47a396476164dad2d211946ed2
Errorsia <Errorsia@outlook.com>
2025-12-20 23:05:01 +0800

    add update state

--- Jiv_enmus.py
+++ Jiv_enmus.py
@@ -4,3 +4,10 @@ class SuspendState(Enum):
     NOT_FOUND = 0
     SUSPENDED = 1
     RUNNING = 2
+
+class UpdateState(Enum):
+    NORMAL = 0
+    FIND_LATEST = 1
+    IS_LATEST = 2
+    NOT_FOUND = 3
+    ERROR = 4
